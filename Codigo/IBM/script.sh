6066fbb8560dff4a12151fc3cb9668f246747622
marioggil <marioggil@gmail.com>
2020-09-28 22:50:19 -0500

    Arreglado codigos y Arreglando presentacion

---
+++ Codigo/IBM/script.sh
@@ -0,0 +1,6 @@
+curl -X POST -u "apikey:rMhlg3v7i7AW3sMMeceji53Qk_Ic5vR-sXtnMvrjb_1x" \
+--header "Content-Type: text/plain;charset=utf-8" \
+--header "Accept: application/json" \
+--header "Content-Language: es" \
+--data-binary @/home/mario/GabrielMarquez.txt \
+"https://api.us-south.personality-insights.watson.cloud.ibm.com/instances/90c4c3f3-21b7-40b0-8a86-4b4d27e333ae/v3/profile?version=2017-10-13"
